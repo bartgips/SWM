9b54befb486290e2b9424540e5e7a4e40d167330
Bart Gips <bart.gips@donders.ru.nl>
2014-05-12 16:56:35 +0200

    bg_swm_extract: added manual Loc input.

--- bg_swm_extract.m
+++ bg_swm_extract.m
@@ -6,12 +6,12 @@ function [z, s, cfg]=bg_swm_extract(cfg, dat)
 %         can contain optional new field: .newLen;
 %         this is the length of the windows that should be cut out of the
 %         dataset (symetrically around the orignal window positions)
-%   
+%
 % dat:    optional; data that the cfg belongs to. (only relevant if cfg
 %         does not contain .varname and .fname
-% 
-% 
-% 
+%
+%
+%
 % last edit: March 2014
 
 if nargin<2
@@ -33,7 +33,7 @@ nanFlag=any(nanSel(:));
 
 if any(isfield(cfg,{'Fbp','Fbs','Fhp','Flp'}))
   disp('Applying frequency filters...')
-%   filttype='fir';
+  %   filttype='fir';
   filttype='but';
   % first temporarily change NaNs to zero's such that filtering works
   if nanFlag
@@ -120,8 +120,6 @@ else
   loc=cfg.loc;
 end
 
-
-
 if isfield(cfg,'best_clust')
   cfg.clust=cfg.best_clust;
   cfg.numclust=numel(cfg.clust);
@@ -138,44 +136,91 @@ else
   clustering=false;
   s=nan(cfg.numTemplates,newLen);
 end
+
+if isfield(cfg,'loc_manual') % manual input of window locations.
+  % loc is now a vector of complex values. Real part: trial index, Imag
+  % part: time index.
+  loc=cfg.loc_manual;
+  if ~iscomplex(loc)
+    error('loc should be a vector of complex values. Real part: trial index, Imag: time index')
+  end
+  % part: time index.
+  manualLoc=true;
+  clustering=false;
+  s=nan(numel(loc),newLen);
+  
+else
+  manualLoc=false;
+end
+
+
 z=s;
 
-if clustering
-  for n=1:numel(s)
-    for k=1:cfg.clust{n}.numTemplates
-      trl=cfg.clust{n}.trl(k);
-      tidx=cfg.clust{n}.tidx(k);
-      if loc(trl,tidx)+winLen+addLen<=size(dat,2) && loc(trl,tidx)-addLen>0
-        s{n}(k,:)=dat(trl,loc(trl,tidx)-addLen:loc(trl,tidx)+winLen+addLen-1);
-      elseif loc(trl,tidx)-addLen>0
-        num=size(dat,2)-loc(trl,tidx)+1;
-        s{n}(k,1:num)=dat(trl,loc(trl,tidx):end);
-      elseif loc(trl,tidx)+addLen<=size(dat,2)
-        num=1-(loc(trl,tidx)-addLen);
-        s{n}(k,num+1:end)=dat(trl,1:loc(trl,tidx)+winLen+addLen-1);
-      end
-    end
-    z{n}=zscore(s{n},1,2);
-  end
-else
-  for k=1:cfg.numTemplates
-    [trl, tidx]=ind2sub(size(loc(:,1:end-1)),k);
-    if loc(trl,tidx)+winLen+addLen<=size(dat,2) && loc(trl,tidx)-addLen>0
-      s(k,:)=dat(trl,loc(trl,tidx)-addLen:loc(trl,tidx)+winLen+addLen-1);
-    elseif loc(trl,tidx)-addLen>0
-      num=size(dat,2)-loc(trl,tidx)+1;
-      s(k,1:num)=dat(trl,loc(trl,tidx):end);
-    elseif loc(trl,tidx)+addLen<=size(dat,2)
-      num=1-(loc(trl,tidx)-addLen);
-      s(k,num+1:end)=dat(trl,1:loc(trl,tidx)+winLen+addLen-1);
+
+
+
+if manualLoc
+  for k=1:numel(loc);
+    trl=real(loc(k));
+    tidx=imag(loc(k));
+    if tidx+winLen+addLen<=size(dat,2) && tidx-addLen>0
+      s(k,:)=dat(trl,tidx-addLen:tidx+winLen+addLen-1);
+    elseif tidx-addLen>0
+      num=size(dat,2)-tidx+1;
+      s(k,1:num)=dat(trl,tidx:end);
+    elseif tidx+addLen+winLen<=size(dat,2)
+      num=1-(tidx-addLen);
+      s(k,num+1:end)=dat(trl,1:tidx+winLen+addLen-1);
+      %     else
+      %       strt=tidx+addLen;
+      %       finish=size(dat,2)+tidx+addLen;
+      %       s(k,strt+1:finish)=dat(trl,1:end);
     end
   end
   z=zscore(s,1,2);
+  
+else
+  if clustering
+    for n=1:numel(s)
+      for k=1:cfg.clust{n}.numTemplates
+        trl=cfg.clust{n}.trl(k);
+        tidx=cfg.clust{n}.tidx(k);
+        if loc(trl,tidx)+winLen+addLen<=size(dat,2) && loc(trl,tidx)-addLen>0
+          s{n}(k,:)=dat(trl,loc(trl,tidx)-addLen:loc(trl,tidx)+winLen+addLen-1);
+        elseif loc(trl,tidx)-addLen>0
+          num=size(dat,2)-loc(trl,tidx)+1;
+          s{n}(k,1:num)=dat(trl,loc(trl,tidx):end);
+        elseif loc(trl,tidx)+addLen<=size(dat,2)
+          num=1-(loc(trl,tidx)-addLen);
+          s{n}(k,num+1:end)=dat(trl,1:loc(trl,tidx)+winLen+addLen-1);
+        end
+      end
+      z{n}=zscore(s{n},1,2);
+    end
+  else
+    for k=1:cfg.numTemplates
+      [trl, tidx]=ind2sub(size(loc(:,1:end-1)),k);
+      if loc(trl,tidx)+winLen+addLen<=size(dat,2) && loc(trl,tidx)-addLen>0
+        s(k,:)=dat(trl,loc(trl,tidx)-addLen:loc(trl,tidx)+winLen+addLen-1);
+      elseif loc(trl,tidx)-addLen>0
+        num=size(dat,2)-loc(trl,tidx)+1;
+        s(k,1:num)=dat(trl,loc(trl,tidx):end);
+      elseif loc(trl,tidx)+addLen+winLen<=size(dat,2)
+        num=1-(loc(trl,tidx)-addLen);
+        s(k,num+1:end)=dat(trl,1:loc(trl,tidx)+winLen+addLen-1);
+        %     else
+        %       strt=loc(trl,tidx)+addLen;
+        %       finish=size(dat,2)+loc(trl,tidx)+addLen;
+        %       s(k,strt+1:finish)=dat(trl,1:end);
+      end
+    end
+    z=zscore(s,1,2);
+  end
 end
 
 
 function z=zscore(x,flag,dim)
-% 
+%
 if nargin<3
   dim=1;
 end
@@ -183,8 +228,8 @@ if nargin<2
   flag=0;
 end
 
-sdx=std(x,flag,dim);
-mux=mean(x,dim);
+sdx=nanstd(x,flag,dim);
+mux=nanmean(x,dim);
 
 z=bsxfun(@minus,x,mux);
 z=bsxfun(@rdivide,z,sdx);
